e6648611c495d2be11fcc602d39d5d7239191dff
Jari Jokinen <jari.jokinen@j2.fi>
2023-07-19 14:13:59 +0300

    Add schema data

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,9 @@
+const schemaPlugin = require('@quasibit/eleventy-plugin-schema');
+
 module.exports = (eleventyConfig) => {
   eleventyConfig.addGlobalData('timestamp', Date.now());
   eleventyConfig.addPassthroughCopy({ 'src/_assets/favicon': '/' });
+  eleventyConfig.addPlugin(schemaPlugin);
 
   return {
     dir: {
